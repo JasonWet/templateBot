41204f966694e25e53ada9a9446eefd505a26349
Jason Dry <jason@colonialpress.net>
2020-04-05 20:12:44 -0400

    Added comments

--- src/commands/testing-test.js
+++ src/commands/testing-test.js
@@ -1,9 +1,10 @@
 module.exports = {
+   //Register a command by the name with a command usage, any aliases (blank if none) and a description
    name: 'test',
    usage: '!test',
    aliases: [''],
    description: 'Test Command',
    async execute (message) {
-
+      //Add the code that gets executed when this command is run below this line.
    },
 };
