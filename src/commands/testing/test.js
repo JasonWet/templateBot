209126cdd34eb180c0b2e5bd03f21a2aa45e72a7
jasonwet <jason@colonialpress.net>
2020-11-01 17:04:58 -0500

    added conf and GuildModel variables

--- src/commands/testing/test.js
+++ src/commands/testing/test.js
@@ -1,7 +1,9 @@
 const Discord = module.require('discord.js');
+const conf = require('../../conf.json')
+const GuildModel = require('../../models/Guild')
 
 module.exports.run = (client, message, args) => {
-    message.channel.send('Test')
+    message.channel.send('TEST')
 }
 
 module.exports.config = {
