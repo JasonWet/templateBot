1a22bbb8849e7eda2201c89f595f3f1bc0ba76e6
jasonwet <jason@colonialpress.net>
2020-11-01 17:05:53 -0500

    updated comments && moved toLowerCase

--- src/events/message.js
+++ src/events/message.js
@@ -1,27 +1,37 @@
 const { client } = require('../index');
-const config = require('../config.json');
+const conf = require('../conf.json');
 
 client.on('message', async (message) => {
-//<==><==><==><==><==><==><==><==><==><==><==><==>COMMAND<==><==><==><==><==><==><==><==><==><==><==><==><==>
+    /*
+        Other Message Events
+     */
+
+        //GO HERE
+
+    /*
+        Execute Command
+     */
     let messageArray = message.content.split(" ");
     let args = messageArray.slice(1);
-//===================================================
-    const prefixes = ['.', `<@${client.user.id}>`]
+
+    const prefixes = [`${conf.prefix}`, `<@${client.user.id}>`]
     let prefix = false;
     for(const thisPrefix of prefixes)
         if(message.content.startsWith(thisPrefix)) prefix = thisPrefix;
-//===================================================
+
     let argument = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = argument.shift().toLowerCase()
 
     let command
     if (client.commands.has(cmd))
         command = client.commands.get(cmd)
-//===================================================
-    message.content = message.content.toLowerCase()
+
+    if (message.content.startsWith(prefix)) {
+        message.content = message.content.toLowerCase()
+    }
     if (!message.content.startsWith(prefix))
         return;
-//===================================================
+
     try {
         command.run(client, message, args, prefix)
     } catch(err) {
