ae625fd471b039244c6f7cfa30eea7c2dfa43efd
jasonwet <jason@colonialpress.net>
2020-11-01 17:11:40 -0500

    removed database variable

--- src/commands/testing/test.js
+++ src/commands/testing/test.js
@@ -1,6 +1,5 @@
 const Discord = module.require('discord.js');
 const conf = require('../../conf.json')
-const GuildModel = require('../../models/Guild')
 
 module.exports.run = (client, message, args) => {
     message.channel.send('TEST')
