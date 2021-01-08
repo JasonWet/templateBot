002ba030a6f4f4be9a445276088275b1a19bcf53
jasonwet <jason@colonialpress.net>
2021-01-08 11:22:29 -0500

    updated index comments

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 // Loads the discord.js library
 const Discord = require("discord.js");
 // Loads other used things in this file
-const { promisify } = require("util");
 const fs = require("fs");
 const Enmap = require("enmap");
 const config = require('./config.json')
-// This is your client. Some people call it `bot`, some people call it `self`,
-// some might call it `cootchie`. Either way, when you see `client.something`,
-// or `bot.something`, this is what we're referring to. Your client.
+// This is your client. Some people call it `bot`
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER']});
 
-// Require our logger
+// Require the Logger
 client.logger = require("./modules/Logger");
 
-// Let's start by getting some useful functions that we'll use throughout
-// the bot, like logs and elevation features.
+// Require and load useful functions used throughout the code
 require("./modules/functions.js")(client);
 
-// Aliases and commands are put in collections where they can be read from,
-// catalogued, listed, etc.
+// Commands and Aliases placed in a collection
 client.commands = new Enmap();
 client.aliases = new Enmap();
 
 const init = async () => {
 
-    // Here we load **commands** into memory, as a collection, so they're accessible
-    // here and everywhere else.
+    /*
+    * LOADING EVENT & COMMANDS
+    *
+    * COMMANDS - Loads our commands and aliases into memory for use all around while the bot is running
+    * EVENTS - Loads our events such as our ready event
+    *
+    */
+    // COMMANDS
+    // Find each sub directory within the commands directory
     await fs.readdirSync("./commands/").forEach(dir => {
         const cmdFiles = fs.readdirSync(`./commands/${dir}/`)
         client.logger.log(`Loading a total of ${cmdFiles.length} commands from the directory ${dir}`);
         cmdFiles.forEach(f => {
+            // Find and sort all command files within the sub directories
             if (!f.endsWith(".js")) return;
             if(cmdFiles.length <= 0) return client.logger.log(`No commands found in ${dir}`)
             try {
@@ -38,30 +41,33 @@ const init = async () => {
                 if (props.init) {
                     props.init(client);
                 }
+                /*
+                    Set the command names & aliases based off of the information provided in the command files
+                */
                 client.commands.set(props.help.name, props);
                 props.conf.aliases.forEach(alias => {
                     client.aliases.set(alias, props.help.name);
                 });
                 return false;
+                // Catch if the command is not able to be loaded
             } catch (e) {
                 return `Unable to load command ${f}: ${e}`;
             }
         });
     });
-    // Then we load events, which will include our message and ready event.
+    // EVENTS
     const evtFiles = await fs.readdirSync("./events/");
     client.logger.log(`Loading a total of ${evtFiles.length} events.`);
+    // Find each event file
     evtFiles.forEach(file => {
         const eventName = file.split(".")[0];
         client.logger.log(`Loading Event: ${eventName}`);
         const event = require(`./events/${file}`);
         // Bind the client to any event, before the existing arguments
-        // provided by the discord.js event.
-        // This line is awesome by the way. Just sayin'.
         client.on(eventName, event.bind(null, client));
     });
 
-    // Here we login the client.
+    // Login to the client using our private token from https://discord.com/developers/applications
     client.login(config.token);
 
 // End top-level async/await function.
