c1cc90033b8a44d5eb55584b49e7c9092e5089e1
jasonwet <jason@colonialpress.net>
2021-01-08 13:00:54 -0500

    updated foramtting

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,9 @@ module.exports = async client => {
     client.logger.log(`${client.user.tag} is now ready. Serving ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s)`, "ready")
 
     // Set the bots activity
-    client.user.setActivity(`Built with TemplateBot`, {type: "CUSTOM_STATUS"})
+    client.user.setActivity(
+        `Built with TemplateBot`,
+        {type: "CUSTOM_STATUS"}
+        );
+
 }
