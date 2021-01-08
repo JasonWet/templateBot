8568ddb693ffb0ec22ca9961de9e630738c87db0
jasonwet <jason@colonialpress.net>
2021-01-08 11:26:37 -0500

    added comments to the commandTemplate.js

--- commands/template/commandTemplate.js
+++ commands/template/commandTemplate.js
@@ -1,13 +1,25 @@
-exports.run = async (client, message, args) => { // eslint-disable-line no-unused-vars
+exports.run = async (client, message, args) => {
+    /*
+        Everything below this comment block is what will be run when a command is run.
+
+        Ex: If someone runs `!template` it will run this file having it send a message to the channel that the command was run in saying `This is a Template Command`
+     */
     message.channel.send("This is a Template Command")
 };
 
+/*
+    Command Configuration
+
+    THE conf & help ARE REQUIRED FOR ALL COMMANDS
+ */
+// Sets if a command is enabled, only able to be used in a guild and any aliases for the command
 exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [],
 };
 
+// Sets the name, category, description and usage example for a command
 exports.help = {
     name: "template",
     category: "Miscelaneous",
